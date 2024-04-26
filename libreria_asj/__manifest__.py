995f726613e6f1bbd0ac20eaae04d90a74c00e9e
Alicia San Julián <aliciasj.2003@gmail.com>
2024-04-26 14:04:12 +0200

    Creada l vista de libro y empezado el security.py

--- libreria_asj/__manifest__.py
+++ libreria_asj/__manifest__.py
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'security/libreria_asj_security.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
